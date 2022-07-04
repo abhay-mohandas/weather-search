56c0376958266906dc038227a83180977fe87e5c
Abhay Mohandas <80393938+abhay-mohandas@users.noreply.github.com>
2022-07-04 21:36:51 +0530

    Update and rename project.py to main.py

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import requests,datetime
 
 
 try:
-    locale_file=open("code.txt")
+    locale_file=open("location.txt")
     api_file=open("weather.api")
 except:
-    print("Location/API details not found!")
-    print("Place the appropriate files in the correct locations")
+    print("""
+!!! Location/API details not found !!!
+Place the appropriate files in the correct locations
+*API file name should be 'weather.api'
+*Location file name should be 'location.txt'""")
     exit()
 
 loc_name,loc_code=[],[]
@@ -21,12 +24,15 @@ for x in locale_file:
     loc_name.append(temp[0])
     loc_code.append(temp[1])
 
-color0="#3f3f3f"
-color1="#cc0000"
+color0="#6f226f"
+color1="#ff2222"
 color2="#1c1c1c"
-color3="#ffffff"
+color3="#2222ff"
 color4="#eeeeee"
-color5="#990000"
+color5="#cc0000"
+color6="#0000cc"
+color7="#55aa55"
+color8="#5555ff"
 
 nil = "< No Information Found >"
 details=''
@@ -34,9 +40,6 @@ details=''
 class Forecast:
     def __init__(self,forecast_info):
         self.forecast=forecast_info['list'] or nil
-
-    def get_info(self):
-        return self.forecast
 
 class Geo:
     def __init__(self,geo_info):
@@ -77,11 +80,10 @@ class Extract:
         self.forecast=Forecast(forecast_info)
     
     def print_info(self):
-        inner_frame.pack(expand=True,fill=BOTH,side=BOTTOM)
+        inner_frame.pack(expand=True,fill=BOTH,side=BOTTOM,padx=1,pady=1)
         Label(  lower_frame,
                 text=" | Location: "+self.geo.name+"| State: "+self.geo.state+"| Country: "+self.geo.country_name+" ("+self.geo.country_code+")"+"| Latitude: "+self.geo.lat+"| Longitude: "+self.geo.lon+"| Timezone: "+self.weather.timezone_actual+"| ",
                 background=color2,foreground=color4,justify=CENTER).pack(anchor="n",padx=2,pady=2,fill=X,side=TOP)
-        
         weather_list=[]
         forecast=details.forecast.forecast
         for x in range(5):
@@ -96,7 +98,7 @@ class Extract:
                     break
                 temp_list.append(temp_forecast)
             weather_list.append(temp_list)
-
+        
         for day_index in range(5):
             weather_day = weather_list.pop(0)
             full_info=""
@@ -105,19 +107,15 @@ class Extract:
                 date_time=x["dt_txt"].split()
                 weather_main=x["weather"][0]["main"]
                 weather_description=x["weather"][0]["description"]
-                temp=x["main"]["temp"]-273.15
-                date_info=date_time[0]
-                full_info+=str(num)+") Time:"+date_time[1][:-3]+" Weather:"+weather_main+" ("+weather_description+")\nTemp:"+str(temp)[:4]+" Celsius\n\n"
+                temp=x["main"]["feels_like"]-273.15
+                date_list=date_time[0].split("-")
+                date_list.reverse()
+                date_info=date_list[0]+"/"+date_list[1]+"/"+date_list[2]
+                full_info+="\n\n"+str(num)+") Time:"+date_time[1][:-3]+"\nWeather: "+weather_main+" ("+weather_description+")\nTemp: "+str(temp)[:4]+" Celsius"
                 num+=1
-            Label(day_list[day_index],text="Date:"+date_info+"\n\n"+full_info,justify=CENTER,foreground=color4,background=color2).pack(anchor="n",side=TOP)
-
-        
-        day1.pack(anchor="nw",side=LEFT,expand=True,fill=BOTH,pady=2,padx=2) 
-        day2.pack(after=day1,anchor="nw",side=LEFT,expand=True,fill=BOTH,pady=2)
-        day3.pack(after=day2,anchor="nw",side=LEFT,expand=True,fill=BOTH,pady=2,padx=2)
-        day4.pack(after=day3,anchor="nw",side=LEFT,expand=True,fill=BOTH,pady=2)
-        day5.pack(after=day4,anchor="nw",side=LEFT,expand=True,fill=BOTH,pady=2,padx=2)
-        
+            Label(day_list[day_index],text="Date:"+date_info+full_info,justify=CENTER,foreground=color4,background=color2).pack(anchor="n",side=TOP)
+        for day in day_list:
+            day.pack(anchor="nw",side=LEFT,expand=True,fill=BOTH,pady=2,padx=1) 
 
 def search_info():
     global details
@@ -125,6 +123,7 @@ def search_info():
     loading.pack_forget()
     warning_empty.pack_forget()
     warning_error.pack_forget()
+    success.pack_forget()
     if not city_info:
         warning_empty.pack()
         return
@@ -138,8 +137,9 @@ def search_info():
         root.update()
         details=Extract(city_info)
         loading.pack_forget()
+        success.pack()
         details.print_info()
-    except ZeroDivisionError():
+    except:
         loading.pack_forget()
         warning_error.pack()
 
@@ -158,26 +158,28 @@ upper_frame=Frame(root,width=200,height=10,bg=color2)
 upper_frame.pack_configure(padx=2,pady=2,side=TOP,fill=X)
 upper_frame.pack()
 
-close=Button(upper_frame,text="X",font=("Sans Bold",7),background=color4,border=0,activebackground=color1,highlightbackground=color1,command=root.destroy)
+close=Button(upper_frame,text="X",font=("Sans Bold",7),background=color1,foreground=color4,border=0,activebackground=color5,activeforeground=color4,command=root.destroy)
 close.pack(pady=0,padx=0,ipady=0,anchor="ne",side=RIGHT)
 
-title=Label(upper_frame,text="        World Weather Search        ",font=("Sans Bold",20),background=color0,foreground=color4,justify=CENTER)
+title=Label(upper_frame,text="        World Weather Search        ",font=("Sans Bold",17),background=color0,foreground=color4,justify=CENTER)
 title.pack(pady=5,anchor="n",side=TOP)
 
-city_text = ttk.Label(upper_frame,text="Enter the location name:",font=("Sans Bold",12),background=color2,foreground=color4)
+city_text = ttk.Label(upper_frame,text="Enter the location name:",font=("Arial",11),background=color2,foreground=color4)
 city_text.pack(anchor="n",padx=50,side=TOP)
 
 city = ttk.Entry(upper_frame,font=("Arial",10),style="Custom.TLabel",justify=CENTER)
+city.focus()
+#city.bind("<Return>",lambda x: search_info)
 city.pack(ipadx=50,pady=5,padx=10,after=city_text,anchor="n")
 
-search=Button(upper_frame,text="Search!",font=("Arial",10),background=color4,borderwidth=0,justify=CENTER,command=search_info)
-search.pack(after=city,padx=10,pady=20,anchor="n")
+search=Button(upper_frame,text="Search!",font=("Arial",10),background=color3,foreground=color4,borderwidth=0,justify=CENTER,activebackground=color6,activeforeground=color4,takefocus=True,command=search_info)
+search.pack(after=city,padx=10,pady=10,anchor="n")
 
-lower_frame=Frame(root,width=1000,height=700,bg=color4)
-lower_frame.pack_configure(after=upper_frame,padx=10,pady=10,expand=True,fill=BOTH)
+lower_frame=Frame(root,width=1000,height=700,bg=color0)
+lower_frame.pack_configure(after=upper_frame,padx=3,pady=3,expand=True,fill=BOTH)
 lower_frame.pack()
 
-inner_frame=Frame(lower_frame,width=1000,height=700,background=color4)
+inner_frame=Frame(lower_frame,width=1000,height=700,background=color0)
 
 day1=Frame(inner_frame,background=color2)
 day2=Frame(inner_frame,background=color2)
@@ -187,8 +189,11 @@ day5=Frame(inner_frame,background=color2)
 
 day_list=[day1,day2,day3,day4,day5]
 
-warning_error=Label(upper_frame,text="Oops! Something went wrong! Try again with a Valid Input or check the Network Connection!",background=color2,foreground=color4,font=("Arial",10))
-warning_empty=Label(upper_frame,text="Input is Empty! Enter a Location name to search",background=color2,foreground=color4,font=("Arial",10))
-loading=Label(upper_frame,text="Please wait! Loading...",background=color2,foreground=color4,font=("Arial",10))
+warning_error=Label(upper_frame,text="Oops! Something went wrong! Try again with a Valid Input or check the Network Connection!",background=color2,foreground=color1,font=("Arial",9))
+warning_empty=Label(upper_frame,text="Input is Empty! Enter a Location name to search",background=color2,foreground=color1,font=("Arial",9))
+loading=Label(upper_frame,text="Please wait! Loading...",background=color2,foreground=color8,font=("Arial",9))
+success=Label(upper_frame,text="Search Successful!",background=color2,foreground=color7,font=("Arial",9))
+
+
 
 root.mainloop()
